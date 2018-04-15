4c216f85e95325cb1ea4ad13781f66a015a0c2e2
xam4lor <xam4lor@gmail.com>
2018-04-15 16:15:38 +0200

    Correction d'un bug
    Correction du bug : pas de redirection lors du click sur le bouton
    "Accueil".

--- inc/html_inc/header/accueil_jouer_contact_recul.php
+++ inc/html_inc/header/accueil_jouer_contact_recul.php
@@ -4,8 +4,8 @@
 
 			<div class="w3-bar" id="myNavbar">
 				<a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-				<a href="../../../" class="w3-bar-item w3-button">ACCUEIL</a>
-				<a href="../../../game/#game" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i>  JOUER</a>
+				<a href="/" class="w3-bar-item w3-button">ACCUEIL</a>
+				<a href="/game/#game" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i>  JOUER</a>
 				<a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i>  CONTACT</a>
 
 				<?php
@@ -15,7 +15,7 @@
 
 			<!-- Navbar sur téléphones -->
 			<div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-				<a href="../../../game/#game" class="w3-bar-item w3-button" onclick="toggleFunction()">JOUER</a>
+				<a href="/game/#game" class="w3-bar-item w3-button" onclick="toggleFunction()">JOUER</a>
 				<a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
 				
 				<?php
--- inc/html_inc/header/accueil_retirerlots_contact.php
+++ inc/html_inc/header/accueil_retirerlots_contact.php
@@ -4,7 +4,7 @@
 
 			<div class="w3-bar" id="myNavbar">
 				<a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-				<a href="../../../" class="w3-bar-item w3-button">ACCUEIL</a>
+				<a href="/" class="w3-bar-item w3-button">ACCUEIL</a>
 				<a href="#lots" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i>  RETIRER VOS LOTS</a>
 				<a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i>  CONTACT</a>
 

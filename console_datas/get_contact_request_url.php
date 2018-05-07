4fab8ff4778039a49a56473efb2eb828650758fb
xam4lor <xam4lor@gmail.com>
2018-05-07 21:44:02 +0200

    Application administratrice
    Ajout du système d'affichage des requêtes de contact pour l'application
    administratrice.

---
+++ console_datas/get_contact_request_url.php
@@ -0,0 +1,55 @@
+<?php
+	include_once '../inc/html_inc/main_php.php';
+
+	if(!isset($_POST['password']) || !isset($_POST['username'])) {
+		?>
+			Vous n'êtes pas autorisé à voir cette page.
+		<?php
+		exit();
+	}
+
+
+	$config = $config->getAppConfig();
+
+	if(
+		strcmp(htmlspecialchars($_POST['username']), $config['user']) != 0
+		|| strcmp(htmlspecialchars($_POST['password']), $config['password']) != 0
+	)
+	{
+		?>
+			Les identifiants de connexion sont incorrects.
+		<?php
+		exit();
+	}
+
+
+	$datas = "{"
+			. "'contact_request' : [";
+
+	$req = $bdd->prepare('SELECT * FROM requetes_contact');
+	$req->execute(array());
+	$first = true;
+
+	while ($donnees = $req->fetch()) {
+		if($first)
+			$first = false;
+		else
+			$datas .= ",";
+
+
+		$datas .=
+				"{"
+					. "'id':'" . $donnees['id'] . "',"
+					. "'pseudo':'" . $donnees['pseudo'] . "',"
+					. "'email':'" . $donnees['email'] . "',"
+					. "'message':'" . $donnees['message'] . "',"
+					. "'date_post':'" . $donnees['date_post'] . "'"
+				. "}";
+	}
+
+	$datas .=
+			 "]"
+		. "}";
+
+
+	echo $datas;
